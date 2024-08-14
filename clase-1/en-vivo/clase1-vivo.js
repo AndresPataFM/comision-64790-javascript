c404bb507cf661097a9b522307eb699c275409a0
AndresPataFM <apfm21@Gmail.com>
2024-08-13 21:07:25 -0300

    clase 1

---
+++ clase-1/en-vivo/clase1-vivo.js
@@ -0,0 +1,125 @@
+// console.log("funciona")
+
+// console.log(1.2-1)
+
+// console.log("hola")
+
+// console.log('juan dijo "hola curso"')
+// “ ” Estas comillas no funcionan en javascript
+
+
+const numerito = 45657.123;
+const primerNumero = 12;
+
+// const segundo-numero = 123; esta nomenclatura no funciona para js
+
+let otroNumero = 24;
+
+let hilo = "hola";
+let hilo2 = "";
+let hilo3 = " ";
+
+const booleano = false;
+
+let noDefinido;
+
+const objeto = null;
+
+console.log(hilo)
+// console.log(otroNumero)
+console.log(typeof hilo)
+// console.log(typeof otroNumero)
+
+// console.log(typeof true)
+
+/* comentario en bloque */
+
+// reasignar valor
+hilo = "chau"
+
+console.log(hilo)
+
+hilo = 123
+
+// se le puede cambiar el tipo de dato
+console.log(hilo)
+
+const dni = 564567
+
+// const hola // tiene que tener valor la const
+
+// const dni = 123123
+
+// var permite cagadas
+// var hola = "hola"
+// var hola = "asd"
+// NO USEN VAR
+
+// var comuflada, esta mal
+// asd = 123
+
+// console.log(asd)
+
+let calculo = 4 + 4*3
+
+console.log(calculo)
+
+console.log(calculo+1)
+console.log(calculo)
+
+calculo = calculo+1
+
+const raro = "hola" + 7 // Qué pasa aca?
+// "hola" + "7" // paso intermedio
+console.log(raro)
+
+// string + nro Lo convierte en un string
+
+console.log("7"+4)
+
+// console.log("hola"-"o")
+
+console.log("chau"-7)
+
+//  NaN - 7
+//  NaN
+
+console.log("123" - 24)
+// 123 - 24
+// 99
+
+// lo hace por más que el string sea número
+
+
+// strings
+
+const nombre = "Andrés";
+
+const saludo = "Hola mi nombre es " + nombre + " un gusto en conocerlos y mi dni es " + dni;
+
+console.log(saludo)
+
+const saludoMejor = `Hola soy ${nombre}, mi dni es ${dni} y esto es 2+2 = ${2+2}`
+
+console.log(saludoMejor)
+
+// const pedirInfo = prompt("Ingrese dato")
+
+// console.log(pedirInfo)
+// console.log(typeof pedirInfo)
+
+// const siNo = confirm("Si o No")
+// console.log(siNo)
+// console.log(typeof siNo)
+
+// const textito = `Usted ingreso ${pedirInfo} y elegio ${siNo}`
+// alert(textito)
+
+console.log("muestro info")
+
+console.warn("cuidado")
+
+console.error("metiste la pata")
+
+
+
